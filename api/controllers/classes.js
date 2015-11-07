1943caf5cb47a9d24097e6f49078387dcb36647e
ZhongyiTong <zhongyi@teambition.com>
2015-11-07 20:56:21 +0800

    Modify apis for swagger intergation.

--- api/controllers/classes.js
+++ api/controllers/classes.js
@@ -5,8 +5,8 @@ let gitlab = require('gitlab')(commons.authObj);
 
 let classes = {};
 
-classes.listAll = () => {
-  return new Promise((resolve, reject) => {
+classes.listAll = (req, res) => {
+  new Promise((resolve, reject) => {
       gitlab.groups.all((groups) => {
         let classes = [];
         for (let group of groups) {
@@ -17,17 +17,58 @@ classes.listAll = () => {
         resolve(classes);
       });
     }
-  );
+  ).then((val) => {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(JSON.stringify(val));
+    res.end();
+  });
 };
 
-classes.get = (id) => {
-  return new Promise((resolve, reject) => {
-      gitlab.groups.show(id, function (group) {
-        console.log(group);
-        resolve(group);
+classes.get = (req, res) => {
+  let id = req.swagger.params.id.value;
+  new Promise((resolve, reject) => {
+    gitlab.groups.show(id, function (group) {
+      resolve(group);
       });
     }
-  );
+  ).then((val) => {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(JSON.stringify(val));
+    res.end();
+  });
+};
+
+classes.listAssignments = (req, res) => {
+  let id = req.swagger.params.id.value;
+  new Promise((resolve, reject) => {
+      gitlab.groups.listProjects(id, function (projects) {
+        let assignments = [];
+        for (let project of projects) {
+
+        }
+        resolve(projects);
+      });
+    }
+  ).then((val)=> {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(JSON.stringify(val));
+    res.end();
+  });
+};
+
+classes.listMaterials = (req, res) => {
+  let id = req.swagger.params.id.value;
+  new Promise((resolve, reject) => {
+      gitlab.groups.listProjects(id, function (group) {
+        if (group.name == 'syllabus')
+          resolve(group);
+      });
+    }
+  ).then((val)=> {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(JSON.stringify(val));
+    res.end();
+  });
 };
 
 module.exports = classes;
